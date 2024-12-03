5d16655cfab3f319e1d5a359aa783235daab84da
aarnold5 <alaina.j.arnold.22@dartmouth.edu>
2024-12-02 23:33:45 -0600

    Day 2 Puzzle 2

--- day2/day2_puzzle1.py
+++ day2/day2_puzzle1.py
@@ -19,8 +19,12 @@ def solve_day2_puzzle1():
                 prev_diff = diff
                 continue
             else:
-                safe = False
-                break
+                for i in range(len(levels)):
+                    if test_safe(levels[0:i] + levels[i+1:len(levels)]):
+                        safe = True
+                        break
+                    else:
+                        safe = False
         
         if safe:
             safe_count += 1
@@ -28,5 +32,23 @@ def solve_day2_puzzle1():
     f.close()
 
     return safe_count
+
+def test_safe(levels):
+    prev_diff = 0
+    for i in range(1, len(levels)):
+        diff = int(levels[i]) - int(levels[i-1])
+        abs_diff = abs(diff)
+    
+        diff_in_range = abs_diff <= 3 and abs_diff >= 1
+        diff_sign_same = (diff > 0 and prev_diff > 0) or (diff < 0 and prev_diff < 0)
+
+        if (i == 1 and diff_in_range) or (i > 1 and diff_sign_same and diff_in_range):
+            prev_diff = diff
+            continue
+        else:
+            return False
+        
+    return True
+
 
 print(solve_day2_puzzle1())
